38253d4512b00ae47243d55e3e555c6b1f78af56
gian-pc <gpaucarcortez@gmail.com>
2024-07-20 21:37:01 -0500

    feat: Antes de IoC
    La creación y gestión de objetos de empleados se realizaba dentro de la clase

---
+++ src/main/java/com/gianpc/IoC/Jefe.java
@@ -0,0 +1,9 @@
+package com.gianpc.IoC;
+
+public class Jefe{
+
+    public String getTareas() {
+        
+        return "Jefe: Gestionar la plantilla de la empresa";
+    }
+}
---
+++ src/main/java/com/gianpc/IoC/UsoEmpleados.java
@@ -0,0 +1,15 @@
+package com.gianpc.IoC;
+
+
+public class UsoEmpleados {
+
+    public static void main(String[] args) {
+        
+        // Creación de objetos de tipo Empleado
+
+        Jefe Empleado1 = new Jefe();
+        System.out.println(Empleado1.getTareas());
+
+    
+    }
+}
